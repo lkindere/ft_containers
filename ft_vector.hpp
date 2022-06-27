a46c0c8297d22552433c4ece9440d02ac4aa4331
lkindere <lkindere@student.42heilbronn.de>
2022-06-27 20:46:13 +0200

    No idea but now in the hpp

--- ft_vector.hpp
+++ ft_vector.hpp
@@ -6,7 +6,7 @@
 /*   By: lkindere <lkindere@student.42heilbronn.    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/06/25 12:37:52 by lkindere          #+#    #+#             */
-/*   Updated: 2022/06/25 19:29:09 by lkindere         ###   ########.fr       */
+/*   Updated: 2022/06/27 20:45:25 by lkindere         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,23 +16,51 @@
 #include <memory>
 
 namespace ft {
+
 template <typename T>
-class vector
+class VectorIterator
 {
 	private:
-		T*					data_;
-		size_t				size_;
-		size_t				capacity_;
-		std::allocator<T>	alloc_;
+		T*	ptr;
+	
 	public:
-		//Constructors
-		vector();
+		VectorIterator();
+		~VectorIterator();
+};
+
+template <typename T, typename Alloc = std::allocator<T> >
+class vector
+{
+	public:
+		typedef T									value_type;
+		typedef Alloc								allocator_type;
+		typedef	typename Alloc::reference			reference;
+		typedef typename Alloc::const_reference		const_reference;
+		typedef typename Alloc::pointer				pointer;
+		typedef typename Alloc::const_pointer		const_pointer;
+		typedef VectorIterator<value_type>			iterator;
+		// typedef typename VectorIterator<const T>	const_iterator;
+		// typedef typename rev_vector_iterator<T>	reverse_iterator;
+		// typedef typename							const_reverse_iterator;
+		typedef	ptrdiff_t							difference_type;
+		typedef	size_t								size_type;
+
+	private:
+		pointer					data_;
+		size_type				size_;
+		size_type				capacity_;
+		allocator_type			alloc_;
+	public:
+		//																		Constructors
+		explicit vector(const allocator_type& alloc = allocator_type()) {};
 		// vector (size_type n, const value_type& val = value_type(),
 		// const allocator_type& alloc = allocator_type());
 		// template <class InputIterator> vector (InputIterator first, InputIterator last,
 		// const allocator_type& alloc = allocator_type());
 		// vector (const vector& x);
-		~vector();
+		// ~vector();
+
+		//																		Iterators
 
 		// //Iterators
 		// iterator begin();
@@ -44,41 +72,129 @@ class vector
       	// reverse_iterator rend();
 		// const_reverse_iterator rend() const;
 
-		// // Capacity
-		size_t size() const;
-		size_t max_size() const;
-		void resize (size_t n, T val);
-		size_t capacity() const;
-		bool empty() const;
-		void reserve (size_t n);
+		// 																		Capacity
+		size_type		size() const { return size_; };
 
-		// // Access
-		T& operator[] (size_t n);
-		const T& operator[] (size_t n) const;
-		T& at (size_t n);
-		const T& at (size_t n) const;
-		T& front();
-		const T& front() const;
-		T& back();
-		const T& back() const;
+		size_type		max_size() const { return alloc_.max_size(); };
 
-		// //Modifiers
+		void			resize (size_type n, value_type val){
+			if (n == size_)
+				return ;
+			if (n < size_){
+				for (;size_ > n; --size_)
+					alloc_.destroy(&data_[size_ - 1]);
+				return ;
+			}
+			if (n < capacity_){
+				for (;size_ < n; ++size_)
+					alloc_.construct(&data_[size_], val);
+				return ;
+			}
+			T	*new_data;
+
+			new_data = alloc_.allocate(n, 0);
+			for (size_t i = 0; i < size_; ++i)
+				new_data[i] = data_[i];
+			alloc_.construct(&new_data[size_], val);
+			alloc_.deallocate(data_, capacity_);
+			data_ = new_data;
+			capacity_ = n;
+			size_ = n;
+		}
+
+		size_type		capacity() const { return capacity_; };
+
+		bool			empty() const { return (size_ == 0); };
+
+		void			reserve (size_type n) {
+			if (n <= capacity_) return;
+			if (capacity_ == 0){
+				data_ = alloc_.allocate(n);
+				capacity_ = n;
+				return ;
+			}
+			if (size_ == 0){
+				alloc_.deallocate(data_, capacity_);
+				data_ = alloc_.allocate(n);
+				capacity_ = n;
+				return ;
+			}
+			T*	new_data;
+
+			new_data = alloc_.allocate(n, 0);
+			for (size_t i = 0; i < size_; ++i)
+				new_data[i] = data_[i];
+			alloc_.deallocate(data_, capacity_);
+			data_ = new_data;
+			capacity_ = n;
+		}
+
+		//																		Access
+		reference		operator[] (size_type n) { return data_[n]; };
+
+		const_reference	operator[] (size_type n) const { return data_[n]; };
+
+		reference		at (size_type n) {
+			if (n >= size_ || n < 0)
+				throw(std::out_of_range("ft_vector"));
+			return (data_[n]);
+		}
+
+		const_reference	at (size_type n) const {
+			if (n >= size_ || n < 0)
+				throw(std::out_of_range("ft_vector"));
+			return (data_[n]);
+		}
+
+		reference		front() { return *data_; };
+
+		const_reference	front() const { return *data_; };
+
+		reference		back() { return data_[size_ - 1]; };
+
+		const_reference	back() const { return data_[size_ - 1]; };
+
+		//																		Modifiers
+
 		// template <class InputIterator> void assign (InputIterator first, InputIterator last); ???
 		// void assign (size_type n, const value_type& val);
-		void push_back (const T& val);
-		void pop_back();
+		void			push_back (const reference val) {
+			if (size_ < capacity_){
+				data_[size_] = val;
+				++size_;
+				return ;
+			}
+			T	*new_data;
+
+			new_data = alloc_.allocate(size_ + 1, 0);
+			for (size_t i = 0; i < size_; ++i)
+				new_data[i] = data_[i];
+			alloc_.construct(&new_data[size_], val);
+			alloc_.deallocate(data_, capacity_);
+			data_ = new_data;
+			++capacity_;
+			++size_;
+		}
+
+		void			pop_back()  {
+			alloc_.destroy(&data_[size_ - 1]);
+			--size_;
+		};
+
 		// iterator insert (iterator position, const value_type& val); ???
     	// void insert (iterator position, size_type n, const value_type& val);
 		// template <class InputIterator> void insert (iterator position, InputIterator first, InputIterator last); ???
 		// iterator erase (iterator position); ???
 		// iterator erase (iterator first, iterator last); ???
 		// void swap (vector& x);
-		void clear();
+
+		void clear() {
+			for (;size_ > 0; --size_)
+				alloc_.destroy(&data_[size_ - 1]);
+		}
 
 		// //Allocator ????
 
 		// //Non member overloads ????
 };
 }
-
-#include "ft_vector.tpp"
--- test.cpp
+++ test.cpp
@@ -6,7 +6,7 @@
 /*   By: lkindere <lkindere@student.42heilbronn.    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/06/25 15:04:17 by lkindere          #+#    #+#             */
-/*   Updated: 2022/06/25 19:28:58 by lkindere         ###   ########.fr       */
+/*   Updated: 2022/06/27 20:33:08 by lkindere         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,259 +16,260 @@
 
 #include "ft_vector.hpp"
 
+
 int	main(void)
 {
-	srand(time(NULL));
+// 	srand(time(NULL));
 
-	ft::vector<int> ftv;
-	std::vector<int> stv;
+// 	ft::vector<int> ftv;
+// 	std::vector<int> stv;
+// // 
+// 	std::cout << "---------------------------------------------------------------\n";
+// 	std::cout << "Init\n";
+// 	std::cout << "---------------------------------------------------------------\n\n";
+// // 
+// 	std::cout << "Max size: " << ftv.max_size() << std::endl;
+// 	std::cout << "Max size: " << stv.max_size() << "\n\n";
+// // 
+// 	std::cout << "Capacity: " << ftv.capacity() << std::endl;
+// 	std::cout << "Capacity: " << stv.capacity() << "\n\n";
+// // 
+// 	std::cout << "Size:     " << ftv.size() << std::endl;
+// 	std::cout << "Size:     " << stv.size() << "\n\n";
+// // 
+// 	std::cout << "Empty:    " << ftv.empty() << std::endl;
+// 	std::cout << "Empty:    " << stv.empty() << "\n\n";
+// // 
+// 	std::cout << "---------------------------------------------------------------\n";
+// 	std::cout << "Reserve 50\n";
+// 	std::cout << "---------------------------------------------------------------\n\n";
 
-	std::cout << "---------------------------------------------------------------\n";
-	std::cout << "Init\n";
-	std::cout << "---------------------------------------------------------------\n\n";
+// 	stv.reserve(50);
+// 	ftv.reserve(50);
 
-	std::cout << "Max size: " << ftv.max_size() << std::endl;
-	std::cout << "Max size: " << stv.max_size() << "\n\n";
+// 	std::cout << "Capacity: " << ftv.capacity() << std::endl;
+// 	std::cout << "Capacity: " << stv.capacity() << "\n\n";
 
-	std::cout << "Capacity: " << ftv.capacity() << std::endl;
-	std::cout << "Capacity: " << stv.capacity() << "\n\n";
-
-	std::cout << "Size:     " << ftv.size() << std::endl;
-	std::cout << "Size:     " << stv.size() << "\n\n";
-
-	std::cout << "Empty:    " << ftv.empty() << std::endl;
-	std::cout << "Empty:    " << stv.empty() << "\n\n";
-
-	std::cout << "---------------------------------------------------------------\n";
-	std::cout << "Reserve 50\n";
-	std::cout << "---------------------------------------------------------------\n\n";
-
-	stv.reserve(50);
-	ftv.reserve(50);
-
-	std::cout << "Capacity: " << ftv.capacity() << std::endl;
-	std::cout << "Capacity: " << stv.capacity() << "\n\n";
-
-	std::cout << "Size:     " << ftv.size() << std::endl;
-	std::cout << "Size:     " << stv.size() << "\n\n";
-
-
-	std::cout << "Empty:    " << ftv.empty() << std::endl;
-	std::cout << "Empty:    " << stv.empty() << "\n\n";
-
-	std::cout << "---------------------------------------------------------------\n";
-	std::cout << "Reserve 200\n";
-	std::cout << "---------------------------------------------------------------\n\n";
-
-	stv.reserve(200);
-	ftv.reserve(200);
-
-	std::cout << "Capacity: " << ftv.capacity() << std::endl;
-	std::cout << "Capacity: " << stv.capacity() << "\n\n";
-
-	std::cout << "---------------------------------------------------------------\n";
-	std::cout << "200 capacity push/display back 5RNG\n";
-	std::cout << "---------------------------------------------------------------\n\n";
-
-	for (int i = 0; i < 5; ++i){
-		int	rng = rand();
-		stv.push_back(rng);
-		ftv.push_back(rng);
-		std::cout << ftv.back() << std::endl;
-		std::cout << stv.back() << "\n\n";
-	}
-
-	std::cout << "Capacity: " << ftv.capacity() << std::endl;
-	std::cout << "Capacity: " << stv.capacity() << "\n\n";
-
-	std::cout << "Size:     " << ftv.size() << std::endl;
-	std::cout << "Size:     " << stv.size() << "\n\n";
+// 	std::cout << "Size:     " << ftv.size() << std::endl;
+// 	std::cout << "Size:     " << stv.size() << "\n\n";
 
 
-	std::cout << "Empty:    " << ftv.empty() << std::endl;
-	std::cout << "Empty:    " << stv.empty() << "\n\n";
+// 	std::cout << "Empty:    " << ftv.empty() << std::endl;
+// 	std::cout << "Empty:    " << stv.empty() << "\n\n";
 
-	std::cout << "---------------------------------------------------------------\n";
-	std::cout << "Reserve 400, redisplay while popback\n";
-	std::cout << "---------------------------------------------------------------\n\n";
+// 	std::cout << "---------------------------------------------------------------\n";
+// 	std::cout << "Reserve 200\n";
+// 	std::cout << "---------------------------------------------------------------\n\n";
 
-	stv.reserve(400);
-	ftv.reserve(400);
+// 	stv.reserve(200);
+// 	ftv.reserve(200);
 
-	for (int i = 0; i < 5; ++i){
-		std::cout << ftv.back() << std::endl;
-		std::cout << stv.back() << "\n\n";
-		ftv.pop_back();
-		stv.pop_back();
-	}
+// 	std::cout << "Capacity: " << ftv.capacity() << std::endl;
+// 	std::cout << "Capacity: " << stv.capacity() << "\n\n";
 
-	std::cout << "Capacity: " << ftv.capacity() << std::endl;
-	std::cout << "Capacity: " << stv.capacity() << "\n\n";
+// 	std::cout << "---------------------------------------------------------------\n";
+// 	std::cout << "200 capacity push/display back 5RNG\n";
+// 	std::cout << "---------------------------------------------------------------\n\n";
 
-	std::cout << "Size:     " << ftv.size() << std::endl;
-	std::cout << "Size:     " << stv.size() << "\n\n";
+// 	for (int i = 0; i < 5; ++i){
+// 		int	rng = rand();
+// 		stv.push_back(rng);
+// 		ftv.push_back(rng);
+// 		std::cout << ftv.back() << std::endl;
+// 		std::cout << stv.back() << "\n\n";
+// 	}
+
+// 	std::cout << "Capacity: " << ftv.capacity() << std::endl;
+// 	std::cout << "Capacity: " << stv.capacity() << "\n\n";
+
+// 	std::cout << "Size:     " << ftv.size() << std::endl;
+// 	std::cout << "Size:     " << stv.size() << "\n\n";
 
 
-	std::cout << "Empty:    " << ftv.empty() << std::endl;
-	std::cout << "Empty:    " << stv.empty() << "\n\n";
+// 	std::cout << "Empty:    " << ftv.empty() << std::endl;
+// 	std::cout << "Empty:    " << stv.empty() << "\n\n";
 
-	std::cout << "---------------------------------------------------------------\n";
-	std::cout << "5RNG push/display back\n";
-	std::cout << "---------------------------------------------------------------\n\n";
+// 	std::cout << "---------------------------------------------------------------\n";
+// 	std::cout << "Reserve 400, redisplay while popback\n";
+// 	std::cout << "---------------------------------------------------------------\n\n";
 
-	for (int i = 0; i < 5; ++i){
-		int	rng = rand();
-		ftv.push_back(rng);
-		stv.push_back(rng);
-		std::cout << ftv.back() << std::endl;
-		std::cout << stv.back() << "\n\n";
-	}
+// 	stv.reserve(400);
+// 	ftv.reserve(400);
 
-	std::cout << "---------------------------------------------------------------\n";
-	std::cout << "Resize 2, redisplay while popback\n";
-	std::cout << "---------------------------------------------------------------\n\n";
+// 	for (int i = 0; i < 5; ++i){
+// 		std::cout << ftv.back() << std::endl;
+// 		std::cout << stv.back() << "\n\n";
+// 		ftv.pop_back();
+// 		stv.pop_back();
+// 	}
 
-	ftv.resize(2, 0);
-	stv.resize(2, 0);
+// 	std::cout << "Capacity: " << ftv.capacity() << std::endl;
+// 	std::cout << "Capacity: " << stv.capacity() << "\n\n";
+
+// 	std::cout << "Size:     " << ftv.size() << std::endl;
+// 	std::cout << "Size:     " << stv.size() << "\n\n";
+
+
+// 	std::cout << "Empty:    " << ftv.empty() << std::endl;
+// 	std::cout << "Empty:    " << stv.empty() << "\n\n";
+
+// 	std::cout << "---------------------------------------------------------------\n";
+// 	std::cout << "5RNG push/display back\n";
+// 	std::cout << "---------------------------------------------------------------\n\n";
+
+// 	for (int i = 0; i < 5; ++i){
+// 		int	rng = rand();
+// 		ftv.push_back(rng);
+// 		stv.push_back(rng);
+// 		std::cout << ftv.back() << std::endl;
+// 		std::cout << stv.back() << "\n\n";
+// 	}
+
+// 	std::cout << "---------------------------------------------------------------\n";
+// 	std::cout << "Resize 2, redisplay while popback\n";
+// 	std::cout << "---------------------------------------------------------------\n\n";
+
+// 	ftv.resize(2, 0);
+// 	stv.resize(2, 0);
 	
-	std::cout << "Size:     " << ftv.size() << std::endl;
-	std::cout << "Size:     " << stv.size() << "\n\n";
+// 	std::cout << "Size:     " << ftv.size() << std::endl;
+// 	std::cout << "Size:     " << stv.size() << "\n\n";
 
-	for (int i = 0; i < 2; ++i){
-		std::cout << ftv.back() << std::endl;
-		std::cout << stv.back() << "\n\n";
-		ftv.pop_back();
-		stv.pop_back();
-	}
+// 	for (int i = 0; i < 2; ++i){
+// 		std::cout << ftv.back() << std::endl;
+// 		std::cout << stv.back() << "\n\n";
+// 		ftv.pop_back();
+// 		stv.pop_back();
+// 	}
 
-	std::cout << "Capacity: " << ftv.capacity() << std::endl;
-	std::cout << "Capacity: " << stv.capacity() << "\n\n";
+// 	std::cout << "Capacity: " << ftv.capacity() << std::endl;
+// 	std::cout << "Capacity: " << stv.capacity() << "\n\n";
 
-	std::cout << "Size:     " << ftv.size() << std::endl;
-	std::cout << "Size:     " << stv.size() << "\n\n";
-
-
-	std::cout << "Empty:    " << ftv.empty() << std::endl;
-	std::cout << "Empty:    " << stv.empty() << "\n\n";
-
-	std::cout << "---------------------------------------------------------------\n";
-	std::cout << "Resize 10 val 42, redisplay while popback\n";
-	std::cout << "---------------------------------------------------------------\n\n";
-
-	ftv.resize(10, 42);
-	stv.resize(10, 42);
-
-	std::cout << "Size:     " << ftv.size() << std::endl;
-	std::cout << "Size:     " << stv.size() << "\n\n";
-
-	for (int i = 0; i < 10; ++i){
-		std::cout << ftv.back() << std::endl;
-		std::cout << stv.back() << "\n\n";
-		ftv.pop_back();
-		stv.pop_back();
-	}
-
-	std::cout << "Capacity: " << ftv.capacity() << std::endl;
-	std::cout << "Capacity: " << stv.capacity() << "\n\n";
-
-	std::cout << "Size:     " << ftv.size() << std::endl;
-	std::cout << "Size:     " << stv.size() << "\n\n";
+// 	std::cout << "Size:     " << ftv.size() << std::endl;
+// 	std::cout << "Size:     " << stv.size() << "\n\n";
 
 
-	std::cout << "Empty:    " << ftv.empty() << std::endl;
-	std::cout << "Empty:    " << stv.empty() << "\n\n";
+// 	std::cout << "Empty:    " << ftv.empty() << std::endl;
+// 	std::cout << "Empty:    " << stv.empty() << "\n\n";
 
-	std::cout << "---------------------------------------------------------------\n";
-	std::cout << "Front\n";
-	std::cout << "---------------------------------------------------------------\n\n";
+// 	std::cout << "---------------------------------------------------------------\n";
+// 	std::cout << "Resize 10 val 42, redisplay while popback\n";
+// 	std::cout << "---------------------------------------------------------------\n\n";
 
-	for (int i = 0; i < 5; ++i){
-		int	rng = rand();
-		ftv.push_back(rng);
-		stv.push_back(rng);
-		std::cout << ftv.front() << std::endl;
-		std::cout << stv.front() << "\n\n";
-		ftv.pop_back();
-		stv.pop_back();
-	}
+// 	ftv.resize(10, 42);
+// 	stv.resize(10, 42);
 
-	std::cout << "Capacity: " << ftv.capacity() << std::endl;
-	std::cout << "Capacity: " << stv.capacity() << "\n\n";
+// 	std::cout << "Size:     " << ftv.size() << std::endl;
+// 	std::cout << "Size:     " << stv.size() << "\n\n";
 
-	std::cout << "Size:     " << ftv.size() << std::endl;
-	std::cout << "Size:     " << stv.size() << "\n\n";
+// 	for (int i = 0; i < 10; ++i){
+// 		std::cout << ftv.back() << std::endl;
+// 		std::cout << stv.back() << "\n\n";
+// 		ftv.pop_back();
+// 		stv.pop_back();
+// 	}
 
+// 	std::cout << "Capacity: " << ftv.capacity() << std::endl;
+// 	std::cout << "Capacity: " << stv.capacity() << "\n\n";
 
-	std::cout << "Empty:    " << ftv.empty() << std::endl;
-	std::cout << "Empty:    " << stv.empty() << "\n\n";
-
-	std::cout << "---------------------------------------------------------------\n";
-	std::cout << "Push_back RNG5, display with at(), []\n";
-	std::cout << "---------------------------------------------------------------\n\n";
-
-	for (int i = 0; i < 5; ++i){
-		int	rng = rand();
-		ftv.push_back(rng);
-		stv.push_back(rng);
-		std::cout << ftv.at(i) << std::endl;
-		std::cout << stv.at(i) << "\n\n";
-		std::cout << ftv[i] << std::endl;
-		std::cout << stv[i] << "\n\n";
-	}
-
-	std::cout << "Capacity: " << ftv.capacity() << std::endl;
-	std::cout << "Capacity: " << stv.capacity() << "\n\n";
-
-	std::cout << "Size:     " << ftv.size() << std::endl;
-	std::cout << "Size:     " << stv.size() << "\n\n";
+// 	std::cout << "Size:     " << ftv.size() << std::endl;
+// 	std::cout << "Size:     " << stv.size() << "\n\n";
 
 
-	std::cout << "Empty:    " << ftv.empty() << std::endl;
-	std::cout << "Empty:    " << stv.empty() << "\n\n";
+// 	std::cout << "Empty:    " << ftv.empty() << std::endl;
+// 	std::cout << "Empty:    " << stv.empty() << "\n\n";
 
-	std::cout << "---------------------------------------------------------------\n";
-	std::cout << "Try catch out of range\n";
-	std::cout << "---------------------------------------------------------------\n\n";
+// 	std::cout << "---------------------------------------------------------------\n";
+// 	std::cout << "Front\n";
+// 	std::cout << "---------------------------------------------------------------\n\n";
 
-	try {ftv.at(5);}
-	catch (std::out_of_range ret) {std::cout << ret.what() << "\n";};
+// 	for (int i = 0; i < 5; ++i){
+// 		int	rng = rand();
+// 		ftv.push_back(rng);
+// 		stv.push_back(rng);
+// 		std::cout << ftv.front() << std::endl;
+// 		std::cout << stv.front() << "\n\n";
+// 		ftv.pop_back();
+// 		stv.pop_back();
+// 	}
 
-	try {stv.at(5);}
-	catch (std::out_of_range ret) {std::cout << ret.what() << "\n\n";};
+// 	std::cout << "Capacity: " << ftv.capacity() << std::endl;
+// 	std::cout << "Capacity: " << stv.capacity() << "\n\n";
 
-	try {ftv.at(-1);}
-	catch (std::out_of_range ret) {std::cout << ret.what() << "\n";};
+// 	std::cout << "Size:     " << ftv.size() << std::endl;
+// 	std::cout << "Size:     " << stv.size() << "\n\n";
 
-	try {stv.at(-1);}
-	catch (std::out_of_range ret) {std::cout << ret.what() << "\n\n";};
 
-	std::cout << "---------------------------------------------------------------\n";
-	std::cout << "RNG10 and clear\n";
-	std::cout << "---------------------------------------------------------------\n\n";
+// 	std::cout << "Empty:    " << ftv.empty() << std::endl;
+// 	std::cout << "Empty:    " << stv.empty() << "\n\n";
 
-	ftv.resize(10, 42);
-	stv.resize(10, 42);
+// 	std::cout << "---------------------------------------------------------------\n";
+// 	std::cout << "Push_back RNG5, display with at(), []\n";
+// 	std::cout << "---------------------------------------------------------------\n\n";
 
-	std::cout << "Capacity: " << ftv.capacity() << std::endl;
-	std::cout << "Capacity: " << stv.capacity() << "\n\n";
+// 	for (int i = 0; i < 5; ++i){
+// 		int	rng = rand();
+// 		ftv.push_back(rng);
+// 		stv.push_back(rng);
+// 		std::cout << ftv.at(i) << std::endl;
+// 		std::cout << stv.at(i) << "\n\n";
+// 		std::cout << ftv[i] << std::endl;
+// 		std::cout << stv[i] << "\n\n";
+// 	}
 
-	std::cout << "Size:     " << ftv.size() << std::endl;
-	std::cout << "Size:     " << stv.size() << "\n\n";
+// 	std::cout << "Capacity: " << ftv.capacity() << std::endl;
+// 	std::cout << "Capacity: " << stv.capacity() << "\n\n";
 
-	std::cout << "Empty:    " << ftv.empty() << std::endl;
-	std::cout << "Empty:    " << stv.empty() << "\n\n";
+// 	std::cout << "Size:     " << ftv.size() << std::endl;
+// 	std::cout << "Size:     " << stv.size() << "\n\n";
 
-	ftv.clear();
-	stv.clear();
 
-	std::cout << "Capacity: " << ftv.capacity() << std::endl;
-	std::cout << "Capacity: " << stv.capacity() << "\n\n";
+// 	std::cout << "Empty:    " << ftv.empty() << std::endl;
+// 	std::cout << "Empty:    " << stv.empty() << "\n\n";
 
-	std::cout << "Size:     " << ftv.size() << std::endl;
-	std::cout << "Size:     " << stv.size() << "\n\n";
+// 	std::cout << "---------------------------------------------------------------\n";
+// 	std::cout << "Try catch out of range\n";
+// 	std::cout << "---------------------------------------------------------------\n\n";
 
-	std::cout << "Empty:    " << ftv.empty() << std::endl;
-	std::cout << "Empty:    " << stv.empty() << "\n\n";
+// 	try {ftv.at(5);}
+// 	catch (std::out_of_range ret) {std::cout << ret.what() << "\n";};
+
+// 	try {stv.at(5);}
+// 	catch (std::out_of_range ret) {std::cout << ret.what() << "\n\n";};
+
+// 	try {ftv.at(-1);}
+// 	catch (std::out_of_range ret) {std::cout << ret.what() << "\n";};
+
+// 	try {stv.at(-1);}
+// 	catch (std::out_of_range ret) {std::cout << ret.what() << "\n\n";};
+
+// 	std::cout << "---------------------------------------------------------------\n";
+// 	std::cout << "RNG10 and clear\n";
+// 	std::cout << "---------------------------------------------------------------\n\n";
+
+// 	ftv.resize(10, 42);
+// 	stv.resize(10, 42);
+
+// 	std::cout << "Capacity: " << ftv.capacity() << std::endl;
+// 	std::cout << "Capacity: " << stv.capacity() << "\n\n";
+
+// 	std::cout << "Size:     " << ftv.size() << std::endl;
+// 	std::cout << "Size:     " << stv.size() << "\n\n";
+
+// 	std::cout << "Empty:    " << ftv.empty() << std::endl;
+// 	std::cout << "Empty:    " << stv.empty() << "\n\n";
+
+// 	ftv.clear();
+// 	stv.clear();
+
+// 	std::cout << "Capacity: " << ftv.capacity() << std::endl;
+// 	std::cout << "Capacity: " << stv.capacity() << "\n\n";
+
+// 	std::cout << "Size:     " << ftv.size() << std::endl;
+// 	std::cout << "Size:     " << stv.size() << "\n\n";
+
+// 	std::cout << "Empty:    " << ftv.empty() << std::endl;
+// 	std::cout << "Empty:    " << stv.empty() << "\n\n";
 
 	// system("leaks a.out");
 	return (0);
